3bb3bc377ebfc7014f26ec7a213305bc813b4d44
hamzaogounaine <hamz2487@gmail.com>
2025-12-17 00:05:32 +0100

    Countries chart added

--- components/app/analyticsPage/CountriesChart.js
+++ components/app/analyticsPage/CountriesChart.js
@@ -124,7 +124,14 @@ export function CountriesChart({data : chartData}) {
           </PieChart>
         </ChartContainer>
       </CardContent>
-      
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 leading-none font-medium">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="text-muted-foreground leading-none">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
     </Card>
   )
 }
